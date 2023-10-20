c0fbdf2e81bcbe5e26675acecdfd9ca6efc1a151
Kader1680 <ouldhenniabaghdad@gmail.com>
2023-10-21 01:07:40 +0200

    adding a new component for the wifi option

--- src/Components/Wifi.jsx
+++ src/Components/Wifi.jsx
@@ -10,9 +10,9 @@ export default function Wifi ()  {
   return (
     
     <div style={{  display: wifi ?  "block" : "none" }} class="global-box">
-            <div className='wifiConnect'>
+            <div class='wifiConnect'>
                 <div  class= "headWifi">
-                    <i className="fa-solid fa-wifi"></i>
+                    <i class="fa-solid fa-wifi"></i>
                     <div style={{ marginLeft:"13px" }}>
                       <h5>AndroidAP345</h5>
                       <p style={{ color:"#c99679" }}>Connected, secured</p><br></br>
@@ -22,7 +22,25 @@ export default function Wifi ()  {
                 <button class= "disConnectBtn" >Disconnect</button>
             </div>
 
-            
+            <div class="Network">
+                <h5 style={{ marginLeft: "10px" }} >Network & Internt settings</h5>
+                <p  style={{ color:"gray", marginLeft: "10px" }}>Change setting sush as making a connection metered</p><br></br>
+                <div class= "box-newtork">
+                    <div>
+                        <i class="fa-solid fa-wifi"></i>
+                        <p>Wifi</p>
+                    </div>
+                    <div>
+                        <i class="fa-solid fa-plane"></i>
+                        <p>Airplane mode</p>
+                    </div>
+                    <div>
+                        <i class="fa-solid fa-wifi"></i>
+                        <p>Mobile htosspot</p>
+
+                    </div>
+                </div>
+            </div>
     </div>
   )
 }
